e3fe69c61d9e64a03d0c56bfb4bbfbbe9a00f7d8
DragonicZero <82735058+DragonicZero@users.noreply.github.com>
2021-04-18 17:31:18 -0400

    Replace Learn React Link with Learn Vue

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vuejs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Vue
         </a>
       </header>
     </div>
